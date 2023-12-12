43c982a4cfc23c3720c22f21668b671a50e4c89f
罗钦雨 <1820851049@qq.com>
2023-12-12 15:32:56 +0800

    refactor the project

--- project_manager.py
+++
@@ -1,8 +0,0 @@
-class ProjectManager:
-    def __init__(self, repo_path=None):
-        # 初始化时需要指定仓库的路径
-        self.repo_path = repo_path or self.get_default_repo_path()
-
-    def get_default_repo_path(self):
-
-        return "/Users/logic/Documents/VisualStudioWorkspace/XAgent-Dev/"
