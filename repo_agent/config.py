b396470501f00a97aacf36120e63e1ce40c6d8b3
Umpire2018 <Arno.Edwards@outlook.com>
2024-01-02 15:18:50 +0800

    fix: correct export_settings to only write modified configurations - Upgrades the Python version to 3.10 to utilize new language features, including the union operator for type hints. - Changes the default model for GPT usage to gpt-3.5-turbo, offering enhanced efficiency and capabilities. - Adapts the handling of model names to support case-insensitivity, providing greater flexibility in model specification. - Adds support for more models - Introduced short aliases for command options to streamline user interactions. - Improved overall user experience and usability of the command-line interface.

--- repo_agent/config.py
+++ repo_agent/config.py
@@ -12,8 +12,14 @@ settings = Dynaconf(
 settings.validators.register(
     Validator("OPENAI_API_KEY", must_exist=True),
 
-    Validator("chat_completion_kwargs.model", must_exist=True, is_in=(
-        "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k", "gpt-4-turbo")),
+    Validator(
+        "chat_completion_kwargs.model",
+        must_exist=True,
+        condition=lambda value: case_insensitive_in(value, [
+            "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-1106",
+            "gpt-4", "gpt-4-32k", "gpt-4-1106-preview",
+        ])
+    ),
 
     # temperature 必须在 0 到 1 之间
     Validator("chat_completion_kwargs.temperature", must_exist=True, 
@@ -30,7 +36,7 @@ settings.validators.register(
 
 def export_settings():
     # 获取当前环境的配置
-    data = settings.as_dict(env=settings.current_env)
+    data = settings.as_dict()
     
     # 将配置写入 SETTINGS_MODULE 指定的文件
     loaders.write(settings.SETTINGS_MODULE, DynaBox(data).to_dict(), merge=False, env=settings.current_env)
@@ -38,7 +44,18 @@ def export_settings():
     # 重新加载配置以应用更新
     settings.reload()
 
-def validate_settings():
+def validate_settings() -> str | None:
+    """
+    验证 Dynaconf settings 中的所有配置。
+
+    这个函数尝试使用 Dynaconf 的 validators 对象的 validate_all 方法
+    来校验配置中的所有设置项。如果有任何验证错误，它会捕获 ValidationError
+    并格式化错误信息。
+
+    返回:
+        str: 如果有验证错误，返回格式化的错误信息字符串；
+             如果没有错误，返回 None。
+    """
     try:
         settings.validators.validate_all()
     except ValidationError as e:
@@ -46,3 +63,17 @@ def validate_settings():
         friendly_errors = [str(message) for _, message in e.details]
         return "\n".join(friendly_errors)
     return None
+
+def case_insensitive_in(value: str, valid_values: list[str]) -> bool:
+    """
+    检查一个字符串值是否存在于一个字符串列表中，不区分大小写。
+
+    参数:
+        value (str): 需要检查的字符串值。
+        valid_values (list of str): 一个包含有效字符串的列表。
+
+    返回:
+        bool: 如果 value（不区分大小写）存在于 valid_values 中，则返回 True；
+              否则返回 False。
+    """
+    return value.lower() in [v.lower() for v in valid_values]
--- repo_agent/main.py
+++ repo_agent/main.py
@@ -1,5 +1,4 @@
 import typer
-from typing import List, Optional
 from typing_extensions import Annotated
 from repo_agent.config import settings, export_settings, validate_settings
 from pathlib import Path
@@ -8,38 +7,75 @@ from loguru import logger
 
 app = typer.Typer()
 
-@app.command()
+
+@app.command(help="Run the documentation generation task.")
 def run():
     errors = validate_settings()
     if errors:
         typer.echo(f"Configuration validation failed with errors: {errors}")
         raise typer.Exit(code=1)
-    
+
     try:
         runner = Runner()
         runner.run()
-        logger.info("文档任务完成。")
+        logger.info("Document task completed.")
     except Exception as e:
-        logger.error(f"运行过程中出现错误: {e}")
+        logger.error(f"Error occurred during execution: {e}")
         raise typer.Exit(code=1)
-    
-@app.command()
+
+
+@app.command(help="Configure the tool's parameter settings.")
 def configure(
-    repo_path: Annotated[str, typer.Option(prompt="Enter the path to your local repository")] = settings.repo_path ,
-    model: Annotated[str, typer.Option(prompt="Enter the model name(recommended: gpt-3.5-turbo-16k)")] = settings.chat_completion_kwargs.model,
-    base_url: Annotated[str, typer.Option(prompt="Enter the base url of model")] = settings.chat_completion_kwargs.base_url,
-    temperature: Annotated[float, typer.Option(prompt="Enter the temperature of model")] = settings.chat_completion_kwargs.temperature,
-    request_timeout: Annotated[int, typer.Option(prompt="Enter the request timeout of model")] = settings.chat_completion_kwargs.request_timeout,
-    project_hierarchy_path: Annotated[str, typer.Option(prompt="Enter the project hierarchy file name")] = settings.project_hierarchy_path,
-    markdown_docs_path: Annotated[str, typer.Option(prompt="Enter the Markdown documents folder name")] = settings.markdown_docs_path,
-    language: Annotated[str, typer.Option(prompt="Enter the language option for the docs")] = settings.language,
-    # TODO Use ignore_list to ignore files or directories
-    #ignore_list: Annotated[Optional[List[str]], typer.Option(prompt="Enter files or directories to ignore")] = settings.ignore_list,
-):  
+        repo_path: Annotated[str, typer.Option(
+            "--repo-path", "-rp",
+            prompt="Enter the path to your local repository",
+            help="The path to your local repository.",
+            show_default=False)] = settings.repo_path,
+        model: Annotated[str, typer.Option(
+            "--model", "-m",
+            prompt="Enter the model name",
+            help="The name of the model to use."
+        )] = settings.chat_completion_kwargs.model,
+        base_url: Annotated[str, typer.Option(
+            "--base-url", "-b",
+            prompt="Enter the base url of model",
+            help="The base URL of the model."
+        )] = settings.chat_completion_kwargs.base_url,
+        temperature: Annotated[float, typer.Option(
+            "--temperature", "-t",
+            prompt="Enter the temperature of model",
+            help="The temperature of the model, affecting the diversity of generated content.")
+        ] = settings.chat_completion_kwargs.temperature,
+        request_timeout: Annotated[int, typer.Option(
+            "--request-timeout", "-rt",
+            prompt="Enter the request timeout of model",
+            help="The request timeout for the model, in seconds."
+        )] = settings.chat_completion_kwargs.request_timeout,
+        project_hierarchy_path: Annotated[str, typer.Option(
+            "--project-hierarchy-path", "-p",
+            prompt="Enter the project hierarchy file name",
+            help="The name of the project hierarchy file."
+        )] = settings.project_hierarchy_path,
+        markdown_docs_path: Annotated[str, typer.Option(
+            "--markdown-docs-path", "-md",
+            prompt="Enter the Markdown documents folder name",
+            help="The folder name for the Markdown documents."
+        )] = settings.markdown_docs_path,
+        language: Annotated[str, typer.Option(
+            "--language", "-l",
+            prompt="Enter the language option for the docs",
+            help="The language option for the documentation."
+        )] = settings.language,
+        # TODO: Use ignore_list to ignore files or directories
+        # ignore_list: Annotated[Optional[List[str]], typer.Option(
+        #     "--ignore-list", "-il",
+        #     prompt="Enter files or directories to ignore",
+        #     help="List of files or directories to ignore."
+        #     )] = settings.ignore_list,
+):
     # 转换为 Path 对象并保存
     path_obj = Path(repo_path)
     settings.repo_path = str(path_obj)
-
     # 更新其他设置
     settings.chat_completion_kwargs.model = model
     settings.chat_completion_kwargs.temperature = temperature
@@ -48,7 +84,7 @@ def configure(
     settings.project_hierarchy_path = project_hierarchy_path
     settings.markdown_docs_path = markdown_docs_path
     settings.language = language
-    #settings.ignore_list = ignore_list
+    # settings.ignore_list = ignore_list
 
     errors = validate_settings()
     if errors:
@@ -57,4 +93,3 @@ def configure(
 
     export_settings()
     typer.echo("Configuration updated and saved.")
-
